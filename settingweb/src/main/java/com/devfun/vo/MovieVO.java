02454435d2a6d0d7f4133392be3e9631ea0aa73b
jjwon915 <jjwon915@naver.com>
2021-09-30 23:37:40 +0900

    [SW 개발 백엔드_1주차 과제]

---
+++ settingweb/src/main/java/com/devfun/vo/MovieVO.java
@@ -0,0 +1,45 @@
+package com.devfun.vo;
+
+public class MovieVO {
+	private int movie_id;
+	private String movie_name;
+	private String director;
+	private String types;
+
+	public MovieVO() {}
+	
+	public MovieVO(int movie_id, String movie_name, String director, String types) {
+		super();
+		this.movie_id = movie_id;
+		this.movie_name = movie_name;
+		this.director = director;
+		this.types = types;
+	}
+	
+	public int getMovie_id() {
+		return movie_id;
+	}
+	public void setMovie_id(int movie_id) {
+		this.movie_id = movie_id;
+	}
+	public String getMovie_name() {
+		return movie_name;
+	}
+	public void setMovie_name(String movie_name) {
+		this.movie_name = movie_name;
+	}
+	public String getDirector() {
+		return director;
+	}
+	public void setDirector(String director) {
+		this.director = director;
+	}
+	public String getTypes() {
+		return types;
+	}
+	public void setTypes(String types) {
+		this.types = types;
+	}
+	
+	
+}
